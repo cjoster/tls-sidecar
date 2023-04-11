97b6fc58d8056fc80614837dacd773d265543513
CJ Oster <cj@cjoster.com>
2023-04-11 16:09:19 -0500

    Adding default env vars

--- docker-entrypoint.d/15-envsubst.sh
+++ docker-entrypoint.d/15-envsubst.sh
@@ -2,4 +2,11 @@
 
 set -euo pipefail
 
+exec >&2
+
+echo "Environment variable \"HTTP_PORT\" is set to \"${HTTP_PORT:=80}\"."
+echo "Environment variable \"HTTPS_PORT\" is set to \"${HTTPS_PORT:=80}\"."
+
+export HTTP_PORT HTTPS_PORT
+
 envsubst '${HTTP_PORT} ${HTTPS_PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf || { >&2 echo "Environment substitution on nginx.conf failed."; exit 1; }
